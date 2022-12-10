e635a517b2cf6fb62370cfdbeb88a34b0f7d4843
Nikolay <shade10@abv.bg>
2022-12-10 18:04:43 +0200

    new featrures

--- scripts/script.js
+++ scripts/script.js
@@ -1,25 +1,66 @@
-import fetchData, { apiBaseUrl, categoriesEndPoint } from "./fetchData.js";
+import fetchData, {
+  apiBaseUrl,
+  categoriesEndpoint,
+  fetchMealsByCategory,
+} from "./fetchData.js";
 import {
   readFromStorage,
   writeToStorage,
   storageKeys,
 } from "./storageControl.js";
+
 const categoriesFilterDiv = document.getElementById(
   "detailed-categories-filter"
 );
+const resultsContainer = document.getElementById("results-contaienr");
+
+function createMealPreviewElement(meal) {
+  const { idMeal, strMealThumb, strMeal } = meal;
+  const recipeDiv = document.createElement("div");
+  recipeDiv.className = "category-box";
+  recipeDiv.setAttribute("id", idMeal);
+
+  const recipeImg = document.createElement("img");
+  recipeImg.setAttribute("src", strMealThumb);
+
+  const recipeTitle = document.createElement("h4");
+  recipeTitle.textContent = strMeal;
+
+  recipeDiv.appendChild(recipeImg);
+  recipeDiv.appendChild(recipeTitle);
+  resultsContainer.appendChild(recipeDiv);
+}
+
+async function showMealsByCategory(category) {
+  const { meals } = await fetchMealsByCategory(category);
+
+  resultsContainer.innerHTML = "";
+
+  meals.forEach((recipe) => {
+    createMealPreviewElement(recipe);
+  });
+
+  if (window.innerHeight < window.innerWidth) {
+    window.scrollTo({ top: 0 });
+  } else {
+    window.scrollTo(0, resultsContainer.offsetTop);
+  }
+}
 
 function createCategoryElement(categoryObj) {
-  const { strCategory, strCategoryThumb } = categoryObj;
+  const { strCategory: title, strCategoryThumb: imgSrc } = categoryObj;
 
   const categoryDiv = document.createElement("div");
   categoryDiv.className = "category-box";
+  categoryDiv.addEventListener("click", () => showMealsByCategory(title));
 
   const categoryThumb = document.createElement("img");
-  categoryThumb.setAttribute("src", strCategoryThumb);
-  categoryThumb.setAttribute("alt", `${strCategory} category image`);
+  categoryThumb.setAttribute("src", imgSrc);
+  categoryThumb.setAttribute("alt", `${title} category image`);
 
   const categoryTitle = document.createElement("h4");
-  categoryTitle.textContent = strCategory;
+  categoryTitle.textContent = title;
+
   categoryDiv.appendChild(categoryThumb);
   categoryDiv.appendChild(categoryTitle);
   return categoryDiv;
@@ -30,16 +71,16 @@ async function main() {
   categories = readFromStorage(storageKeys.categories);
 
   if (!categories) {
-    const { categories: fetchedCategoreis } = await fetchData(
-      apiBaseUrl + categoriesEndPoint
+    const { categories: remoteCategories } = await fetchData(
+      apiBaseUrl + categoriesEndpoint
     );
-    categories = fetchedCategoreis;
+    categories = remoteCategories;
     writeToStorage(storageKeys.categories, categories);
   }
 
-  categories.forEach((element) => {
-    const newCategoryElement = createCategoryElement(element);
-    categoriesFilterDiv.appendChild(newCategoryElement);
+  categories.forEach((el) => {
+    const newCategoryEl = createCategoryElement(el);
+    categoriesFilterDiv.appendChild(newCategoryEl);
   });
 }
 
--- scripts/storageControl.js
+++ scripts/storageControl.js
@@ -1,19 +1,14 @@
-// 1. Read from storage
-// 2. Write to storage
-const storageType = "sessionStorage";
+const storageType = "localStorage";
 const storageKeys = {
   categories: "categories",
   areas: "areas",
 };
-
 function readFromStorage(key) {
   const stringValue = window[storageType].getItem(key);
-  const parseValue = JSON.parse(stringValue);
-  return parseValue;
+  const parsedValue = JSON.parse(stringValue);
+  return parsedValue;
 }
-
 function writeToStorage(key, value) {
   window[storageType].setItem(key, JSON.stringify(value));
 }
-
 export { readFromStorage, writeToStorage, storageKeys };
